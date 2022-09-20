d1d781f4e5a1043a5428e86314d9654ded04798c
glebite <glebite@gmail.com>
2022-09-20 19:33:06 -0400

    managed to get RGB tuple translated into a dictionary key that then got put into the Counter class.

--- src/iconizer.py
+++ src/iconizer.py
@@ -1,17 +1,22 @@
 from PIL import Image
-import io
-import pathlib
 from collections import Counter
+
 
 with Image.open("icon.png") as img:
     img.load()
 
-print(dir(img))
-width, height = img.size
+def colour_weight(img):
+    width, height = img.size
+    print(type(img))
 
-c = Counter()
-for i in range(0, width):
-    for j in range(0, height):
-        c.update((img.getpixel((i,j))))
+    c = Counter()
+    for i in range(0, width):
+        for j in range(0, height):
+            d = dict()
+            d[img.getpixel((i,j))] = 1
+            c.update(d)
+    print(c)
 
-print(c)
+
+if __name__ == "__main__":
+    main()
