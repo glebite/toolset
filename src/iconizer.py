f135e74f8cf7e93bd139bc28454836df5c1d4397
glebite <glebite@gmail.com>
2022-09-21 21:33:17 -0400

    Going to throw in some elisp code

--- src/iconizer.py
+++ src/iconizer.py
@@ -2,21 +2,29 @@ from PIL import Image
 from collections import Counter
 
 
-with Image.open("icon.png") as img:
+with Image.open("Green.png") as img:
     img.load()
 
-def colour_weight(img):
+def colour_histogram(img):
+    """
+    """
     width, height = img.size
-    print(type(img))
 
-    c = Counter()
-    for i in range(0, width):
-        for j in range(0, height):
-            d = dict()
-            d[img.getpixel((i,j))] = 1
-            c.update(d)
-    print(c)
+    histogram = Counter()
+    for width_ptr in range(0, width):
+        for height_ptr in range(0, height):
+            pixel_value = dict()
+            pixel_value[img.getpixel((width_ptr, height_ptr))] = 1
+            histogram.update(pixel_value)
+    return histogram
 
 
+def main():
+    with Image.open("Green.png") as img:
+        img.load()
+    histo = colour_histogram(img)
+    print(histo)
+
+    
 if __name__ == "__main__":
     main()
