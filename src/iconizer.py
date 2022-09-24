5506559642e6b31bbb8d5bb5b939e061442fe69f
glebite <glebite@gmail.com>
2022-09-23 22:43:47 -0400

    Detecting rectangles/squares.

--- src/iconizer.py
+++ src/iconizer.py
@@ -1,9 +1,9 @@
-from PIL import Image
+import sys
 from collections import Counter
+from PIL import Image
+import cv2 as cv
+import numpy
 
-
-with Image.open("Green.png") as img:
-    img.load()
 
 def colour_histogram(img):
     """
@@ -19,17 +19,33 @@ def colour_histogram(img):
     return histogram
 
 
-def find_rectangles(img):
+def find_rectangles(image):
     """
     find rectangles and then predominant colours in them.
     """
+    img = cv.cvtColor(numpy.array(image), cv.COLOR_RGB2BGR)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    Blur = cv.GaussianBlur(gray, (5,5), 1)
+    Canny = cv.Canny(Blur, 20, 50)
+    contours = cv.findContours(Canny,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)[0]
+    cntrRect = []
+    for i in contours:
+        epsilon = 0.05*cv.arcLength(i,True)
+        approx = cv.approxPolyDP(i,epsilon,True)
+        if len(approx) == 4:
+            cv.drawContours(img,cntrRect,-1,(0,255,0),2)
+            cv.imshow('Roi Rect ONLY',img)
+            cntrRect.append(approx)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    print(contours)
 
 
 def main():
-    with Image.open("Green.png") as img:
+    with Image.open(sys.argv[1]) as img:
         img.load()
-    histo = colour_histogram(img)
-    print(histo)
+    find_rectangles(img)
 
     
 if __name__ == "__main__":
