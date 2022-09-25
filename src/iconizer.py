f98c5111eef52f0836391d4aec6389442d4fe96e
glebite <glebite@gmail.com>
2022-09-24 23:53:44 -0400

    More work

--- src/iconizer.py
+++ src/iconizer.py
@@ -2,7 +2,7 @@ import sys
 from collections import Counter
 from PIL import Image
 import cv2 as cv
-import numpy
+import numpy as np
 
 
 def colour_histogram(img):
@@ -19,33 +19,53 @@ def colour_histogram(img):
     return histogram
 
 
-def find_rectangles(image):
+def find_rectangles(image, min_thresh, max_thresh, kernel, iterations):
     """
     find rectangles and then predominant colours in them.
     """
-    img = cv.cvtColor(numpy.array(image), cv.COLOR_RGB2BGR)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    Blur = cv.GaussianBlur(gray, (5,5), 1)
-    Canny = cv.Canny(Blur, 20, 50)
-    contours = cv.findContours(Canny,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)[0]
-    cntrRect = []
-    for i in contours:
-        epsilon = 0.05*cv.arcLength(i,True)
-        approx = cv.approxPolyDP(i,epsilon,True)
-        if len(approx) == 4:
-            cv.drawContours(img,cntrRect,-1,(0,255,0),2)
-            cv.imshow('Roi Rect ONLY',img)
-            cntrRect.append(approx)
+    
+    image = cv.imread(image)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    blur = cv.medianBlur(gray, 5)
+    sharpen_kernel = np.array([[-1,-1,-1], [-1,kernel,-1], [-1,-1,-1]])
+    sharpen = cv.filter2D(blur, -1, sharpen_kernel)
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    print(contours)
+    # Threshold and morph close
+    thresh = cv.threshold(sharpen, min_thresh, max_thresh, cv.THRESH_BINARY_INV)[1]
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+    close = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel, iterations=iterations)
 
+    # Find contours and filter using threshold area
+    cnts = cv.findContours(close, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    min_area = 100
+    max_area = 182
+    image_number = 0
+    found = 0
+    for c in cnts:
+        area = cv.contourArea(c)
+        x, y, w, h = cv.boundingRect(c)
+        
+        # if area > min_area and area < max_area:
+        #     print(area, c)
+        #     found += 1
+        if 10 <= w <= 15 and 10 <= h <= 15:
+            found += 1
+            cv.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+            image_number += 1
+    if found >= 4:
+        ROI = image[y:y+h, x:x+w]
+        cv.imwrite(f'ROI_{min_thresh}_{max_thresh}_{kernel}_{iterations}.png')
+    return found
+ 
 
 def main():
-    with Image.open(sys.argv[1]) as img:
-        img.load()
-    find_rectangles(img)
+    for iterations in range(0,5):
+        for kernel in range(5,15):
+            for min in range(0, 255):
+                for max in range(255, 0, -1):
+                    val = find_rectangles(sys.argv[1], min, max, kernel, iterations)
 
     
 if __name__ == "__main__":
